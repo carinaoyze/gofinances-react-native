12ae8a1a07c418381076da7d737b20a3d4daf46a
Carina Yanaze <carinaoyanaze@gmail.com>
2022-01-11 09:05:37 -0300

    asyncstorage

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {Container, Header, Title} from './styles';
+import {Container, Header, Title, Content} from './styles';
+//import { VictoryPie } from 'victory-native';
 
 import { HistoryCard } from '../../components/HistoryCard';
+import { categories } from '../../utils/categories';
 
-export interface TransactionData{
+interface TransactionData{
+   
     type: 'positive' | 'negative';
     name: string;
     amount: string;
@@ -12,15 +15,56 @@ export interface TransactionData{
     date: string;
 }
 
+interface CategoryData{
+    key: string;
+    name: string;
+    total: number;
+    totalFormatted: string;
+    color: string;
+}
 
 export function Resume(){
+
+    const [totalByCategories, settotalByCategories] = useState<CategoryData[]>([]);
 
     async function loadData(){
         const dataKey = '@gofinances:transactions';
         const response = await AsyncStorage.getItem(dataKey);
         const responseFormatted = response ? JSON.parse(response) : [];
 
-        const expensives = responseFormatted.filter((expensive: TransactionData) => expensive.type === 'negative')
+        const expensives = responseFormatted
+        .filter((expensive: TransactionData) => expensive.type === 'negative');
+
+        const totalByCategory:CategoryData[] = [];
+    
+        categories.forEach(category => {
+            let categorySum = 0;
+            expensives.forEach((expensive: TransactionData)=>{
+                if(expensive.category === category.key){
+                    categorySum += Number(expensive.amount);
+                }
+            });
+
+            if(categorySum > 0) {
+
+                const totalFormatted = categorySum
+                .toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+                });
+
+                totalByCategory.push({
+                    key: category.key,
+                    name: category.name,
+                    total: categorySum,
+                    totalFormatted,
+                    color: category.color
+                })
+            }
+        })
+
+        settotalByCategories(totalByCategory);
+
     }
 
     useEffect(()=>{
@@ -33,11 +77,22 @@ export function Resume(){
             <Header>
                 <Title>Resumo por categoria</Title>
             </Header>
-            <HistoryCard 
-                title="Compras"
-                amount="R$ 150,0 "
-                color="red"
-            />
+            <Content >
+                {/* <VictoryPie
+                    data={totalByCategories}
+                    x="name"
+                    y="total"
+                /> */}
+                {   totalByCategories.map(item => (
+                        <HistoryCard 
+                        key={item.key}
+                        title={item.name}
+                        amount={item.totalFormatted}
+                        color={item.color}
+                        />
+                    ))
+                }
+            </Content>
         </Container>
     )
 }
